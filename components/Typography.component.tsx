81ef71edf2e951545a4502e00d2db19b1a61a367
Benjamin Schachter <benschac@gmail.com>
2021-06-17 22:08:01 -0700

    refactor: remove and clean up code

--- components/Typography.component.tsx
+++ components/Typography.component.tsx
@@ -1,7 +1,7 @@
 import { styled, baseTypography } from "../theme";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text as RawText } from "react-native";
 
-export const TextPrimary = styled(Text)`
+export const Text = styled(RawText)`
   ${baseTypography}
   ${({ theme, weight }) =>
     `font-family: ${theme.fonts["open-sans"][weight ?? "regular"]}`}
@@ -10,7 +10,8 @@ export const TextPrimary = styled(Text)`
     `border: solid ${StyleSheet.hairlineWidth}px orange;`}
 `;
 
-export const DisplayPrimary = styled(Text)`
+//
+export const Display = styled(RawText)`
   ${baseTypography}
   ${({ theme, weight }) =>
     `font-family: ${theme.fonts["roboto-slab"][weight ?? "regular"]}`}
--- screens/Settings.screen.tsx
+++ screens/Settings.screen.tsx
@@ -2,10 +2,7 @@ import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { Layout } from "../components/Layout.component";
 import { Spacer } from "../components/Spacer.component";
-import {
-  DisplayPrimary,
-  TextPrimary,
-} from "../components/Typography.component";
+import { Display, Text } from "../components/Typography.component";
 
 export const SettingsScreen: React.FC = () => {
   const navigation = useNavigation();
@@ -13,8 +10,8 @@ export const SettingsScreen: React.FC = () => {
   return (
     <Layout.ScreenContainer bg="white" grow justify>
       <Layout.PressableColumn px py onPress={() => navigation.navigate("Home")}>
-        <DisplayPrimary size="xl-28">I am the Settings Screen</DisplayPrimary>
-        <TextPrimary size="l-24">Press Me!</TextPrimary>
+        <Display size="xl-28">I am the Settings Screen</Display>
+        <Text size="l-24">Press Me!</Text>
       </Layout.PressableColumn>
       <Layout.Column>
         <Spacer.Vertical size="l-24" />
